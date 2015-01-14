ffb18ba28f5c73996761a4fb61e000c393e4842b
Jose Luis <sisjosex@gmail.com>
2015-01-14 07:54:10 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -65,6 +65,23 @@ function write(path, content){
     }, fail);
 }
 
+function convertImgToBase64(url, callback, outputFormat){
+    var canvas = document.createElement('CANVAS');
+    var ctx = canvas.getContext('2d');
+    var img = new Image;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img,0,0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+        callback.call(this, dataURL);
+        // Clean up
+        canvas = null;
+    };
+    img.src = url;
+}
+
 function gotFS(fs) {
 
     fileSystem = fs;
